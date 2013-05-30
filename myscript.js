10f83a9d98c7dbf139fcb7ceb9d33621935e8d84
pestao <inertiatic@yandex.ru>
2013-05-30 21:29:33 +0300

    changed myscript.js

--- myscript.js
+++ myscript.js
@@ -4,26 +4,26 @@ $(function(){
         return;
     var artist = productHeader.find('#product-header-artist').attr('data-product_artist');
     var title = productHeader.find('#product-header-title').attr('data-product_title');
-    var searchURL = 'http://glorybeats.com/?s=' + encodeURIComponent(artist + ' ' + title);
-    var searchElem = $('<a href="' + searchURL + '">')
-                    .text('Download')
-                    .addClass('mini-summary')
-                    .attr('target', '_blank')
-                    .css({
-                        'position' : 'absolute',
-                        'top' : '20px',
-                        'right' : '10px'
-                    });
-    var searchURL2 = 'http://nodata.tv/?s=' + encodeURIComponent(artist + ' ' + title);
-    var searchElem2 = $('<a href="' + searchURL2 + '">')
-                    .text('Download')
-                    .addClass('mini-summary')
-                    .attr('target', '_blank')
-                    .css({
-                        'position' : 'absolute',
-                        'top' : '20px',
-                        'right' : '10px'
-                    });
+    
+    var glorybeats = 'http://glorybeats.com/?s=' + encodeURIComponent(artist + ' ' + title);
+    var nodata = 'http://nodata.tv/?s=' + encodeURIComponent(artist + ' ' + title);
+    
+    var SearchElem = function(URL){
+        this.elem = $('<a href="' + URL + '">');
+    };
+    SearchElem.prototype.addElem = function() {
+        this.elem.text('Download')
+        .addClass('mini-summary')
+        .attr('target', '_blank')
+        .css({
+            'position' : 'absolute',
+            'top' : '20px',
+            'right' : '10px'
+        });
+    };
+    var searchGB = new SearchElem(glorybeats);
+    var searchND = new SearchElem(nodata);
+
     var searchErrorElem = $('<div>Download unavailable</div>')
                     .attr({'id' : 'product-header-catalog-number'})
                     .css({
@@ -32,17 +32,17 @@ $(function(){
                         'top' : '10px',
                         'right' : '10px'
                     });
-    $.get(searchURL, function(data){
+    $.get(glorybeats, function(data){
         if (data.indexOf('No matches') != -1) {
-            $.get(searchURL, function(data){
+            $.get(nodata, function(data){
             if (data.indexOf('Our Apologies') != -1) {
-                productHeader.find('#product-header-body').append(searchElem2);
+                productHeader.find('#product-header-body').append(searchND.addElem);
             } else {
                 productHeader.find('#product-header-body').append(searchErrorElem);
             };
         });
         } else {
-            productHeader.find('#product-header-body').append(searchElem);
+            productHeader.find('#product-header-body').append(searchGB.addElem);
         }
     });
 });
